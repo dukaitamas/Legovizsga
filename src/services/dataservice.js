8e5913890718605b5ed731716430c864e515be51
Dukai Tamás <dukai.tamas@student.jedlik.eu>
2024-05-06 18:02:20 +0200

    működik de nem írja ki a legördülő mellet a számokat

--- src/services/dataservice.js
+++ src/services/dataservice.js
@@ -5,6 +5,17 @@ export default {
   getAllThemes() {
     return Axios.get("/themes")
       .then((resp) => {
+        //console.log(resp.data);
+        return resp.data;
+      })
+      .catch((err) => {
+        //console.log(err);
+        return Promise.reject(err);
+      });
+  },
+  getAllSets() {
+    return Axios.get("/sets")
+      .then((resp) => {
         console.log(resp.data);
         return resp.data;
       })
@@ -13,32 +24,21 @@ export default {
         return Promise.reject(err);
       });
   },
-  getAllValami() {
-    return Axios.get("/valamik")
-      .then((resp) => {
-        //console.log(resp.data);
-        return resp.data;
-      })
-      .catch((err) => {
-        //console.log(err);
-        return Promise.reject(err);
-      });
-  },
-  getValamiById(id) {
-    return Axios.get(`/valamik/${id}`)
-      .then((resp) => {
-        //console.log(resp.data);
-        return resp.data;
-      })
-      .catch((err) => {
-        //console.log(err);
-        return Promise.reject(err);
-      });
-  },
-  saveUser(újobjektum) {
-    return Axios.post("/valamik", újobjektum).then(() => {});
-  },
-  updateUser(módosítottObjektum, id) {
-    return Axios.put(`/valamik/${id}`, módosítottObjektum).then(() => {});
-  },
+  // getValamiById(id) {
+  //   return Axios.get(`/valamik/${id}`)
+  //     .then((resp) => {
+  //       //console.log(resp.data);
+  //       return resp.data;
+  //     })
+  //     .catch((err) => {
+  //       //console.log(err);
+  //       return Promise.reject(err);
+  //     });
+  // },
+  // saveUser(újobjektum) {
+  //   return Axios.post("/valamik", újobjektum).then(() => {});
+  // },
+  // updateUser(módosítottObjektum, id) {
+  //   return Axios.put(`/valamik/${id}`, módosítottObjektum).then(() => {});
+  // },
 };
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,9 +4,9 @@ import { ref, onMounted } from "vue";
 
 const kategoriak = ref([]);
 const valamik = ref([]);
-const valasztottKategoriaNev = ref();
+const keszletek = ref([]);
 const valasztottKategoriaId = ref();
-const kiválasztottValamik = ref([]);
+const kiválasztottKeszletek = ref([]);
 
 onMounted(() => {
   // Ez akkor fut le, amikor betöltődik a HomeView komponens
@@ -14,43 +14,45 @@ onMounted(() => {
   DataService.getAllThemes()
     .then((resp) => {
       kategoriak.value = resp;
-      console.log(kategoriak.value);
+      //console.log(kategoriak.value);
     })
     .catch((err) => {
       console.log(err);
     });
 
-// DataService.getAllValami()
-//   .then((resp) => {
-//     valamik.value = resp;
-//     //console.log(valamik.value);
-//   })
-//   .catch((err) => {
-//     console.log(err);
-//   });
+DataService.getAllSets()
+  .then((resp) => {
+    keszletek.value = resp;
+    //console.log(keszletek.value);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
-// const valaszto = () => {
-//   // Ha a backend-en nincs olyan végpont, ami csak a kiválasztott id-jű elemeket adja vissza,
-//   // akkor az összes elemet le kell kérni és kiválogatni közülük a megfelelőket:
-//   valasztottKategoriaId.value = kategoriak.value.find(
-//     (k) => k.nev === valasztottKategoriaNev.value
-//   ).id;
-//   console.log(valasztottKategoriaId.value);
-//   kiválasztottValamik.value = valamik.value.filter(
-//     (v) => v.kategoriaId === valasztottKategoriaId.value
-//   );
-//   console.log(kiválasztottValamik.value);
+const valaszto = () => {
+  console.log("ok");
+  // Ha a backend-en nincs olyan végpont, ami csak a kiválasztott id-jű elemeket adja vissza,
+  // akkor az összes elemet le kell kérni és kiválogatni közülük a megfelelőket:
+  // valasztottKategoriaId.value = kategoriak.value.find(
+  //   (k) => k.nev === valasztottKategoriaNev.value
+  // ).id;
+   console.log(keszletek.value);
+  kiválasztottKeszletek.value = keszletek.value.filter(
+    (v) => v.themeId === valasztottKategoriaId.value
+  );
+  console.log(kiválasztottKeszletek.value);
 
   // ha van megfelelő végpont (és kellene, hogy legyen) akkor használjuk azt (:
-//};
+};
 </script>
 
 <template>
-  <select v-model="valasztottKategoriaNev" @change="valaszto">
-    <option v-for="kategoria in kategoriak">{{ kategoria.name }}</option>
+  <select v-model="valasztottKategoriaId" @change="valaszto">
+    <option v-for="kategoria in kategoriak" :value="kategoria.id">{{ kategoria.name }}</option>
+    <!-- //:value="kategoria.id az id számait írja ki nem a nevét -->
   </select>
   {{valasztottKategoriaNev}}
   <ul class="m-4">
-    <li v-for="valami in kiválasztottValamik">{{ valami.nev }}</li>
+    <li v-for="keszlet in kiválasztottKeszletek">{{ keszlet.setName }}</li>
   </ul>
 </template>
